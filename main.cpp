d1d25c575669e106063b79ad7f285af6619d3e64
Zach Clayburn <zachclayburn@gmail.com>
2017-09-18 13:30:30 -0600

    Added comand line arguments

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+using namespace std;
+
+int main(int nargin, char* args[]) {
+
+	if(nargin != 2){
+		cout << "Error: This program takes exactly one argument, the file to be read." << endl;
+		return 0;
+	}
+
+	return 0;
 }
